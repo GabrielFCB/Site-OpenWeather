bc6b86376abcac3427d593580d4358cd829f5e7f
Gabriel Feitosa <gabrielfcb13@gmail.com>
2023-11-16 11:34:46 -0300

    Colocar data da apicall na pagina

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
 // Funções
@@ -19,16 +19,28 @@ const getWeatherData = async (city) => {
 
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
 
-    const res = await fetch (apiWeatherURL)
-    const data= await res.json()
+    const res = await fetch (apiWeatherURL);
+    const data= await res.json();
 
-    console.log(data)
+    return(data);
 }
 
 
 
-const showWeatherData = (city)=> {
-    getWeatherData(city);
+const showWeatherData = async (city)=> {
+    const data = await getWeatherData(city);
+    cityElement.innerText= data.name;
+    tempElement.innerText= parseInt(data.main.temp);
+    descElement.innerText= data.weather[0].description;
+    weatherIconElement.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    const flagIconURL= apiCountryURL1+data.sys.country+apiCountryURL2;
+    countryElement.setAttribute("src",flagIconURL);
+    humidityElement.innerText= data.main.humidity+"%";
+    windElement.innerText= data.wind.speed+"km/h"; 
+
+
+    console.log(data)
+    
 }
 
 //Eventos
