d87702a55bb26314e9f91a8f83b92926b90ac613
Gabriel Feitosa <gabrielfcb13@gmail.com>
2023-11-16 11:03:12 -0300

    Adicionar javascript com chamada da API

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-//f8a52d7883969f5dcbe05d54d94ddc55
+//Variáveis e seleção de elementos
+const apiKey="f8a52d7883969f5dcbe05d54d94ddc55";
+const apiCountryURL1 ="https://flagsapi.com/";
+const apiCountryURL2 ="/shiny/64.png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+// Funções
+const getWeatherData = async (city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch (apiWeatherURL)
+    const data= await res.json()
+
+    console.log(data)
+}
+
+
+
+const showWeatherData = (city)=> {
+    getWeatherData(city);
+}
+
+//Eventos
+searchBtn.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    const city= cityInput.value;
+
+    showWeatherData(city);
+
+})
